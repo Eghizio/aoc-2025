805a60649d9b1bedf82c3a9d5d065eba9157cf7f
Jakub Wąsik <jakubwasik@Jakubs-MacBook-Pro.local>
2025-12-01 07:27:25 +0100

    Day 01 - TypeScript

--- typescript/day-01/part-1.ts
+++ typescript/day-01/part-1.ts
@@ -1,7 +1,28 @@
-import { loadInput, sum, toInt } from "../utils";
+import { loadInput, toInt } from "../utils";
+
+const parseRotation = (text: string): number => {
+  const value = toInt(text.slice(1));
+  return text.startsWith("L") ? -value : value;
+};
+
+const MAX = 99 + 1;
+const rotate = (position: number, rotation: number) =>
+  (MAX + position + rotation) % MAX;
+
+const countZeros = (rotations: number[]) => {
+  const [position, zeros] = rotations.reduce(
+    ([position, zeros], rotation) => {
+      const next = rotate(position, rotation);
+      return [next, zeros + (next === 0 ? 1 : 0)];
+    },
+    [50, 0]
+  );
+
+  return zeros;
+};
 
 const input = loadInput();
-
 const lines = input.split("\n");
+const rotations = lines.map(parseRotation);
 
-console.log(lines);
+console.log(countZeros(rotations));
